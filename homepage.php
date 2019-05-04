2bd5b37d9876bd40c5130e17f4145fd3416051bb
BuiNam988 <49862425+BuiNam988@users.noreply.github.com>
2019-05-04 10:40:36 +0700

    aaa

---
+++ homepage.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Company ATN</title>
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="web.css">
+</head>
+<body>
+	<nav class="navbar navbar-inverse sticky" >
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span> 
+				</button>
+				<a class="navbar-brand" href="Company ATN.html"><p>Company ATN</p></a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+				</ul>
+				
+				</div>
+		</div>
+	</nav>
+	<!--  -->
+	<div class="container-fluid " style="margin-bottom: : 10px">
+		<div class="row">
+			<!--  -->
+			<div class="col-sm-6">
+				<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="3000">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+						<li data-target="#myCarousel" data-slide-to="1"></li>
+						<li data-target="#myCarousel" data-slide-to="2"></li>
+					</ol>
+
+					
+						<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="right carousel-control" href="#myCarousel" data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
+				</div>
+				<!--  -->
+			</div>
+			
+	
+	<div class="row text-center">
+			<div class=" col-sm-3">
+				<div class="panel panel-default">
+					<div class="panel-heading"><b>Robot Armen</b></div>
+					<div class="panel-body">
+						<div ><img src="Images/na1.jpg" style="">
+						</div>
+						<div class="text-center"><b>Price: 150$</b></div>
+					</div>
+				</div>
+			</div>
+			<div class=" col-sm-3">
+				<div class="panel panel-default">
+					<div class="panel-heading"><b>Robos Gao</b></div>
+					<div class="panel-body">
+						<div ><img src="Images/na2.jpg" style="">
+						</div>
+						<div class="text-center"><b>Price: 150$</b></div>
+					</div>
+				</div>
+			</div>
+			<div class=" col-sm-3">
+				<div class="panel panel-default">
+					<div class="panel-heading"><b>Batmen</b></div>
+					<div class="panel-body">
+						<div ><img src="Images/na3.jpg" style="">
+						</div>
+						<div class="text-center"><b>Price: 150$</b></div>
+					</div>
+				</div>
+			</div>
+			<div class=" col-sm-3">
+				<div class="panel panel-default">
+					<div class="panel-heading"><b>HEroku</b></div>
+					<div class="panel-body">
+						<div ><img src="Images/na4.jpg" style="">
+						</div>
+						<div class="text-center"><b>Price: 150$</b></div>
+					</div>
+				</div>
+			</div>
+			<div class=" col-sm-3">
+				<div class="panel panel-default">
+					<div class="panel-heading"><b>Maslow</b></div>
+					<div class="panel-body">
+						<div ><img src="Images/na5.jpg" style="">
+						</div>
+						<div class="text-center"><b>Price: 150$</b></div>
+					</div>
+				</div>
+			</div>
+			<div class=" col-sm-3">
+				<div class="panel panel-default">
+					<div class="panel-heading"><b>Robot Lipice</b></div>
+					<div class="panel-body">
+						<div ><img src="Images/na9.jpg" style="">
+						</div>
+						<div class="text-center"><b>Price: 150$</b></div>
+					</div>
+				</div>
+			</div>
+			<div class=" col-sm-3">
+				<div class="panel panel-default">
+					<div class="panel-heading"><b>Robot Nivia</b></div>
+					<div class="panel-body">
+						<div ><img src="Images/na7.jpg" style="">
+						</div>
+						<div class="text-center"><b>Price: 150$</b></div>
+					</div>
+				</div>
+			</div>
+			<div class=" col-sm-3">
+				<div class="panel panel-default">
+					<div class="panel-heading"><b>Robot Omomatic</b></div>
+					<div class="panel-body">
+						<div ><img src="Images/na8.jpg" style="">
+						</div>
+						<div class="text-center"><b>Price: 150$</b></div>
+					</div>
+				</div>
+			</div>
+			
+
+	</body>
+</html>
